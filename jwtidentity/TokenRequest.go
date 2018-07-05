9774a596bf746f2f17517e56f77ee57e95573ed0
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-05 11:17:22 +1000

    jwt

--- jwtidentity/TokenRequest.go
+++ jwtidentity/TokenRequest.go
@@ -5,8 +5,8 @@ type TokenRequest struct {
 	UserARN     string   `json:"user_arn"`
 	ClientID    uint64   `json:"client_id"`
 	ClientName  string   `json:"client_name"`
-	FirstName   string   `json:"first_name"`
-	LastName    string   `json:"last_name"`
+	FirstName   *string  `json:"first_name"`
+	LastName    *string  `json:"last_name"`
 	Username    string   `json:"username"`
 	Groups      []string `json:"groups"`
 	Permissions []string `json:"permissions"`
